d322b38be6330192ed4abb273dcc72fad9dce058
teresethulin <terese.thulin@gmail.com>
2019-12-02 19:58:40 +0100

    Create cards

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict"
+
+const cards = document.querySelectorAll('.card');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
