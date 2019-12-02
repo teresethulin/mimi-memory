600f88f1a1c9c9c7769daf188fa722ffe61053c8
teresethulin <terese.thulin@gmail.com>
2019-12-02 21:02:54 +0100

    Add images to cards

--- script.js
+++ script.js
@@ -2,8 +2,27 @@
 
 const cards = document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        //first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // second click
+        hasFlippedCard = false;
+        secondCard = this;
+    }
+
+    console.log({
+        firstCard,
+        secondCard
+    });
+
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
